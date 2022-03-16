79c4181d4e8c5d9454a1c876e084762a584c18ef
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-16 18:13:45 +0000

    basics working

--- guess the number.py
+++ guess the number.py
@@ -1,5 +1,5 @@
 import random
-number = random.rand7int(1, 10)
+number = random.randint(1, 10)
 
 #set count variable to 0
 count = 0
@@ -14,7 +14,31 @@ while True:
     print("..............................................")
     print()
 
-    #see if their number is too highand tell them
+    #if their number is too high and tell them
     if userguess > number:
         time.sleep(1)
+        print("Nope, that's too high!")
+
+    #if their number is too low and tell them
+    if userguess < number:
+        time.sleep(1)
         print("Nope, that's too low!")
+
+    #if the number is tha same say so, and break cycle
+    if userguess == number:
+        print("You have guessed correct")
+        
+        #every time you went through the cyle it added 1 to 'count'
+        print("You've had", count, "attempts.")
+        print()
+        time.sleep(1)
+        print("Goodbye!")
+        break
+
+    #if they have too many guesse the cycle breaks
+    elif count == 5:
+        print("You have ran out of guesses :[")
+        print()
+        time.sleep(1)
+        print("Goodbye!")
+        break
