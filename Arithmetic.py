66b1c44922ae76a6959e9e83635733d24a5effff
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-16 18:32:56 +0000

    bad calc adittion done

---
+++ Arithmetic.py
@@ -0,0 +1,7 @@
+#basically just a caculator but bad for some reason
+
+print("Addition")
+number1 = float(input("Enter first number here: "))
+number2 = float(input("Enter second number here: "))
+print(number1 + number2)
+print(".................................................")
