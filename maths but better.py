eb177fec6003c3fb9a4c3865a0fa2f9809848fba
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-16 22:01:24 +0000

    fixed the timing

--- maths but better.py
+++ maths but better.py
@@ -1,4 +1,5 @@
 #The arithmetic code but better :]
+import time
 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("CALCULATOR-INATOR!")
@@ -21,6 +22,7 @@ time.sleep(4)
 
 print("############################################")
 print("INSTRUCTIONS!")
+print("-------------")
 print()
 time.sleep(1)
 print("We only have a few to make your expierience")
@@ -31,30 +33,32 @@ time.sleep(3)
 print("""
 (1) Only use numbers, our program isn't
     equipped to solve algebra""")
-time.sleep(2)
+time.sleep(4)
 print("""
 (2)When you enter the menu you can pick what 
     setting you would like to use.""")
-time.sleep(2)
+time.sleep(4)
 print("""
 (3)If you have entered a setting you didnt intened
     to access you can type 'BACK' which will take
     you back to the main menu""")
+time.sleep(5)
 print("""
 (4) This technology is only for: addition
     multiplication, subtraction, and
     division. Anything else will be deleted.""")
-time.sleep(2)
+time.sleep(5)
 print("""
 (5) Don't blame us if anything doesn't work
     as we are a small company, with limited
     resources.""")
-time.sleep(3)
+time.sleep(4)
 print()
 print("That's all and you can continue! :]")
 print()
 print("############################################")
 
+time.sleep(3)
 print()
 print("MAIN MENU")
 print("Welcome to the menu!")
