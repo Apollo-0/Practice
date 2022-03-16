b82abf30de03577f2d6392fb5455e39676176c7b
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-16 21:31:33 +0000

    math but good

---
+++ maths but better.py
@@ -0,0 +1,18 @@
+#The arithmetic code but better :]
+
+print("CALCULATOR-INATOR!")
+print("Welcome to calculator-inator the number 1")
+print("interactive and sentient calculator")
+time.sleep
+print()
+print("As long as you follow our simple rules")
+print("everything should go smoothly :]")
+
+
+print(".................................................")
+print("Addition")
+number1 = float(input("Enter first number here: "))
+number2 = float(input("Enter second number here: "))
+print(number1 + number2)
+print(".................................................")
+time.sleep(1)
