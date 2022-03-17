5e815c9bb186bc35ed576a584700d87ce108b789
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-17 18:23:03 +0000

    rps base started

---
+++ rock paper scissors.py
@@ -0,0 +1,29 @@
+#Rock paper scissors game
+
+#all the scores and imports
+import sys
+import random
+import time
+rnd = 0
+score = 0
+uS = 0
+cS = 0
+
+print("....................................")
+print("!!ROCK PAPER SCISSORS SHOOT!!")
+print("....................................")
+print()
+time.sleep(2)
+
+#the computer and users choices
+choice = ['rock', 'paper', 'scissors']
+
+print("RULES")
+print("The only rule is to type in all lowercase")
+time.sleep(1)
+print("Also make sure your spellings are correct!")
+time.sleep(2)
+print("Have fun! :]")
+print()
+print(".................................................")
+print()
