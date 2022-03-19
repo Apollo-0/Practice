6183200298572c37993fa458a789f8dd071fc789
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-19 22:45:47 +0000

    half done!

--- phone number checker.py
+++ phone number checker.py
@@ -1,5 +1,6 @@
 #Phone Number Checker
 
+import time
 numbers = "0123456789"
 
 #set flag to be True
@@ -19,4 +20,15 @@ while True:
 
     #first number should be a 0
     if phonenumber[0] != "0":
-        valid 
+        valid = Falseprint("Your first number doesn't start with 0")
+        time.sleep(1)
+        print("Please try again")
+        print()
+
+    #second number should be 7
+    if phonenumber[1] != "7":
+        valid = Falseprint("Your second number isn't a 7")
+        time.sleep(1)
+        print("Please try again")
+        print()
+        
