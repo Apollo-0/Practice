ff13a5ebefae05772941d46114437af89937a0b1
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-03-31 19:37:43 +0000

    started

---
+++ snakes and ladders.py
@@ -0,0 +1,10 @@
+#snakes and ladders game
+import time
+
+
+
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+print("                  Welcome to...")
+time.sleep(2)
+print("               SNAKES AND LADDERS!!")
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
