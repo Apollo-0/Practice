af5c4ce533253288d660c9f57327918c35b3eb24
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-06-06 19:32:20 +0000

    i honestly cant be bothered

---
+++ Improvments - Mercury/listing menu options.py
@@ -0,0 +1,33 @@
+#A more efficient way to pick food off the menu
+
+import time
+number = []
+
+print("YOOOOO WELCOME!!!!11!")
+
+while True:
+    print("Please pick an option:")
+    print()
+    time.sleep(1)
+    print("Option: A")
+    print()
+    time.sleep(1)
+    print("Option: B")
+    print()
+    time.sleep(1)
+
+    option = input("Your Option is: ")
+    print()
+    time.sleep(1)
+
+    if option == 'A':
+        print("You have chosen option A")
+
+        while True:
+            print("number 1")
+            print()
+            print("number 2")
+            print()
+            print("number 3")
+
+            number1 == input("Your number was:", [])
